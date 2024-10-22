fa91a785a2d0edb0b82565abda0b9580691ff2fc
EM-R-M <emily.manton@aol.com>
2024-10-22 11:11:53 +0100

    Sakila questions attempted

--- Sakila Answers.sql
+++ Sakila Answers.sql
@@ -75,3 +75,46 @@ INNER JOIN category ON film_category.category_id = category.category_id
 GROUP BY category.name;
 
 -- Q17: List all movies featuring a robot.
+SELECT * FROM film
+WHERE description LIKE "%robot%";
+
+-- Q18: How many movies were released in 2010?
+SELECT COUNT(*) FROM film
+WHERE release_year = 2010;
+
+-- Q19: Find the titles of all the horror movies.
+SELECT title FROM film 
+INNER JOIN film_category ON film.film_id = film_category.film_id
+INNER JOIN category ON film_category.category_id = category.category_id
+WHERE category.name = "Horror";
+
+-- Q20: List the full name of the staff member with the ID of 2.
+SELECT first_name, last_name FROM staff
+WHERE staff_id LIKE "%2%";
+
+-- Q21: List all the movies that Fred Costner has appeared in.
+SELECT title FROM actor
+INNER JOIN film_actor ON actor.actor_id = film_actor.actor_id
+INNER JOIN film ON film_actor.film_id = film.film_id
+WHERE first_name = "Fred" AND last_name = "Costner";
+
+-- Q22: How many distinct countries are there?
+SELECT DISTINCT country FROM country;
+
+-- Q23: List the name of every language in reverse-alphabetical order.
+SELECT name FROM language
+ORDER BY name DESC;
+
+-- Q24: List the full names of every actor whose surname ends with '-son' in 
+--      alphabetical order by their forename.
+SELECT first_name, last_name FROM actor
+WHERE last_name LIKE "%son"
+ORDER BY first_name ASC;
+
+-- Q25: Which category contains the most films?
+SELECT name FROM category
+WHERE category_id = (SELECT category_id FROM film
+						INNER JOIN film_category ON film.film_id = film_category.film_id
+						GROUP BY category_id
+						ORDER BY COUNT(category_id) DESC
+						LIMIT 1);
